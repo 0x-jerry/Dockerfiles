a154bd4b2912706aa54b6cb136e19bbb36b62d49
Gem <x.li.gem@gmail.com>
2020-05-30 00:31:35 -0900

    update config

--- tinyfilemanager/src/tinyfilemanager.php
+++ tinyfilemanager/src/tinyfilemanager.php
@@ -25,8 +25,8 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
-    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
+    'admin' => '$2y$10$5t3PiNZ7wJEYmO9yr84L5uUU4bXaZ.5bzhZzf5cCVr7yZwIajrXFe', //cwxyz007
+    'user' => '$2y$10$o5kt5rOozX8JpDju/ywQnOV1j0y11OED5asynnl.7076yFIkVXkpy' //1hu$rSMoS9
 );
 
 //set application theme
